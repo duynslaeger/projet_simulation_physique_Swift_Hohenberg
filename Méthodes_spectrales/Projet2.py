9db9f0db0a0eba4274bbf3fe79ebf813285b2c2d
Sacha Duynslaeger <sacha.duynslaeger@student.uclouvain.be>
2022-05-11 11:38:39 +0200

    ajout code graphe dependance

--- Méthodes_spectrales/Projet2.py
+++ Méthodes_spectrales/Projet2.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# ------------------- Fonction qui calcule la soution pour un r et un L spécifique -----------------
+
+
 def compute(r, L):
 
     N = 1024
@@ -49,6 +52,9 @@ dt = 0.05
 Nt = int(200 / dt)
 t = np.linspace(0, 200, Nt)
 
+
+# ------------ Plot la figure de l'énoncé -------------
+"""
 un = compute(r, L)
 
 c = np.linspace(np.min(un[-1]), np.max(un[-1]), 101)
@@ -61,91 +67,107 @@ plt.ylabel('t')
 plt.ylim(0, 175)
 plt.title("r = {}, L = {}".format(r, L))
 plt.show()
+"""
 
-# ---------------- Plot dépendance ----------------
 
-# # Dépendance en r
+# ------------------------- Plot dépendance -------------------------
 
-# variation amplitude
+
+# ---------------- Amplitude en fonction de r ----------------
+
+"""
 amplitudes = []
+rs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+
+for k in range(len(rs)):
+    U = compute(rs[k], L)
+    amplitudes.append(np.max(U[125]))
+
+plt.plot(rs, amplitudes, '-o')
+plt.title("Variation de l'amplitude du champ de température en fonction de r")
+plt.xlabel('r')
+plt.ylabel('Amplitude de u(x,t)')
+plt.show()
+
+"""
+
+# ---------------- Amplitude en fonction de L ----------------
+
+"""
+
+Ls = [100, 125, 150, 175, 200]
 lengths = []
-# rs = [0.1,0.2,0.3,0.4,0.5,0.6,0.7]
-# Ls = [100,125,150,175, 200]
 
-# for k in range(len(rs)) :
-#     U = compute(rs[k], L)
-#     amplitudes.append(np.max(U[125]))
-
-# for k in range(len(Ls)) :
-#     U = compute(0.2,Ls[k])
-#     lengths.append(np.max(U[200]))
+for k in range(len(Ls)):
+    U = compute(0.2, Ls[k])
+    lengths.append(np.max(U[200]))
 
 
-# plt.plot(rs,amplitudes,'-o')
-# plt.title("Variation de l'amplitude du champ de température en fonction de r")
-# plt.xlabel('r')
-# plt.ylabel('Amplitude de u(x,t)')
-# plt.show()
+plt.plot(Ls, lengths, '-o')
+plt.title("Variation de l'amplitude du champ de température en fonction de L")
+plt.xlabel('L')
+plt.ylabel('Amplitude de u(x,t)')
+plt.ylim(-0.01, 0.08)
+plt.show()
+
+"""
 
 
-# plt.plot(Ls,lengths,'-o')
-# plt.title("Variation de l'amplitude du champ de température en fonction de L")
-# plt.xlabel('L')
-# plt.ylabel('Amplitude de u(x,t)')
-# plt.ylim(-0.01,0.08)
-# plt.show()
+# ---------------- Longueur d'onde en fonction de L ----------------
 
-# Ls = [10, 50, 100, 125, 150, 175, 200]
-# rs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
-# lambd = []
+"""
+Ls = [10, 50, 100, 125, 150, 175, 200]
+lambdL = []
 
 
-# oldMax = 0
-# # -------------- /!\ ATTENTION !! UTILISER np.round(numer, numberof_decimals) POUR MATCHER LE MAX
-# for k in range(len(Ls)):
-#     count = 0
-#     Ul = compute(0.2, Ls[k])
-#     x = np.linspace(0, Ls[k] - Ls[k] / N, N)
-#     lim = np.round(np.max(Ul[-1]), 2)
-#     print("max = ", lim)
-#     for j in range(len(Ul[-1])):
-#         # print(Ul[-1][j])
-#         if((np.round(Ul[-1][j], 2) == lim) & count == 1):
-#             print("here")
-#             print("x[j] = ", x[j])
-#             lambd.append(x[j] - oldMax)
-#             break
-#         if((np.round(Ul[-1][j], 2) == lim) & count == 0):
-#             print("oldMax = ", oldMax)
-#             oldMax = x[j]
-#             count += 1
+oldMax = 0
+for k in range(len(Ls)):
+    count = 0
+    Ul = compute(0.2, Ls[k])
+    x = np.linspace(0, Ls[k] - Ls[k] / N, N)
+    lim = np.round(np.max(Ul[-1]), 2)
+    for j in range(len(Ul[-1])):
+        if((np.round(Ul[-1][j], 2) == lim) & count == 1):
+            lambdL.append(x[j] - oldMax)
+            break
+        if((np.round(Ul[-1][j], 2) == lim) & count == 0):
+            oldMax = x[j]
+            count += 1
+
+plt.plot(Ls, lambdL, '-o')
+plt.title("Variation de la longueur d'onde en fonction de L")
+plt.xlabel('L')
+plt.ylabel('$\lambda$')
+plt.show()
+"""
+
+# ---------------- Longueur d'onde en fonction de r ----------------
+
+"""
+rs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+lambdr = []
+
+oldMax = 0
+for k in range(len(rs)):
+    count = 0
+    Ur = compute(0.2, rs[k])
+    x = np.linspace(0, rs[k] - rs[k] / N, N)
+    lim = np.round(np.max(Ur[-1]), 2)
+    for j in range(len(Ur[-1])):
+        if((np.round(Ur[-1][j], 2) == lim) & count == 1):
+            lambdr.append(x[j] - oldMax)
+            break
+        if((np.round(Ur[-1][j], 2) == lim) & count == 0):
+            oldMax = x[j]
+            count += 1
 
 
-# # Ur = compute(rs[k], 100)
-# # lengths.append(np.max(Ul[200]))
-# # amplitudes.append(np.max(Ur[125]))
-# print(lambd)
-# print(Ls)
 
-# plt.plot(Ls, lambd, '-o')
-# plt.title("Variation de la longueur d'onde en fonction de L")
-# plt.xlabel('L')
-# plt.ylabel('$\lambda$')
-# plt.show()
+plt.plot(rs, lambdr, '-o')
+plt.title("Variation de la longueur d'onde en fonction de r")
+plt.xlabel('r')
+plt.ylim(-0.2, 0.2)
+plt.ylabel('$\lambda$')
+plt.show()
 
-# print(lambd)
-# print(rs)
-
-# plt.plot(rs, lambd, '-o')
-# plt.title("Variation de la longueur d'onde en fonction de r")
-# plt.xlabel('r')
-# plt.ylim(-0.2, 0.2)
-# plt.ylabel('$\lambda$')
-# plt.show()
-
-# plt.plot(Ls,lengths,'-o')
-# plt.title("Variation de l'amplitude du champ de température en fonction de L")
-# plt.xlabel('L')
-# plt.ylabel('Amplitude de u(x,t)')
-# plt.ylim(-0.01,0.08)
-# plt.show()
+"""
